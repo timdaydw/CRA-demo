809c1f0c4264ed5422e4416f5c5447a566b70824
timdaydw <162669638+timdaydw@users.noreply.github.com>
2024-06-05 14:38:40 +0800

    0605

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+// 外部檔案
+import axios from 'axios';
+
 import logo from '../css/logo.svg';
 import '../css/App.css';
 import Input from './Input';
+import '../css/all.css';
+
+
+import $ from "jquery";
+
 
 function App() {
-  const [inputValue,setInputValue] =useState(" ") ;
-  const [inputValue2,setInputValue2] =useState(" ") ;
+  const [inputValue, setInputValue] = useState(" ");
+  const [inputValue2, setInputValue2] = useState(" ");
+  
+  // 網頁初始化(開啟網頁時執行)
+  useEffect(() => {
+    (async () => {
+      const result = await axios.get('https://randomuser.me/api/');
+      console.log(result);
+    })();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +29,11 @@ function App() {
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
         </p>
+        <button type="button" class="btn btn-primary" onClick={() => {
+          $(function () {
+            alert("jquery")
+          })
+        }}>Primary</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,12 +42,12 @@ function App() {
         >
           Learn React
         </a>
-        <Input id="testId" text="USER ID" value={inputValue} onChangeHandler={(e)=>{
+        <Input id="testId" text="USER ID" value={inputValue} onChangeHandler={(e) => {
           setInputValue(e.target.value);
-        }}/>
-        <Input id="testId2" text="USER ID2" value={inputValue2} onChangeHandler={(e)=>{
+        }} />
+        <Input id="testId2" text="USER ID2" value={inputValue2} onChangeHandler={(e) => {
           setInputValue2(e.target.value);
-        }}/>
+        }} />
       </header>
     </div>
   );
